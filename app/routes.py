1eb0d23a0d9170437919be95983da40bc9e64cf5
j-max <jmax.barry@gmail.com>
2024-03-24 12:16:56 -0500

    Add templates to show log of folds on home page

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,30 @@
+from flask import render_template
 from app import app
+
 
 @app.route("/")
 @app.route("/index")
 def index():
-    return "Hello, World!"
+    user = {"username": "Max"}
+    folds = [
+        {
+            "designer": "Jason Schneider",
+            "design": "Toad",
+            "paper_type": "Kami",
+            "paper_size": "6x6",
+            "date_folded": "3/23/24"
+        },
+        {
+            "designer": "Akira Yoshizawa",
+            "design": "Pinwheel",
+            "paper_type": "Kami",
+            "paper_size": "6x6",
+            "date_folded": "3/22/24"
+        }
+    ]
+    return render_template(
+        "index.html",
+        title="Keep Folding!",
+        user=user,
+        folds=folds
+    )
